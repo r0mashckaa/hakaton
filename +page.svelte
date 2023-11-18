02e4c4e0b14f54977cd1641dcd4d68c9abdd3c2b
r0mashcka <145837752+r0mashckaa@users.noreply.github.com>
2023-11-18 21:36:22 +0500

    Add files via upload

---
+++ +page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Form from "$lib/Components/Form.svelte"
+</script>
+<Form />
+
+<style>
+	:global(body) {
+		box-sizing:border-box ;
+	}
+	:global(*) {
+		box-sizing: inherit;
+	}
+</style>
